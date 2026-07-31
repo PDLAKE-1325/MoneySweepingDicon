2d2847ea94e0ca46816670bb938c6970976c0e64
PDLAKE-1325 <163096820+PDLAKE-1325@users.noreply.github.com>
2026-07-31 22:43:38 +0900

    a

--- Assets/Scripts/Managers/BattleManager.cs
+++ Assets/Scripts/Managers/BattleManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
@@ -8,14 +9,14 @@ public class BattleManager : MonoBehaviour
     public static BattleManager Instance { get; private set; }
     public BattleUIManager UIManager { get; private set; }
 
-    CancellationTokenSource _cancelSource = new();
+    public const int MaxPlayerUnits = 5;
+    public const int MaxEnemyUnits = 5;
+    CancellationTokenSource _cancelSource;
 
-    [SerializeField] Transform[] _playerPos;
-    [SerializeField] Transform[] _enemyPos;
-    [SerializeField] BattleUnit[] _playerUnit;
-    [SerializeField] BattleUnit[] _enemyUnit;
+    [SerializeField] Transform[] _playerPos = new Transform[MaxPlayerUnits];
+    [SerializeField] Transform[] _enemyPos = new Transform[MaxEnemyUnits];
 
-#region Unity Methods
+    #region Unity Methods
     void Awake()
     {
         Instance = this;
@@ -27,16 +28,61 @@ public class BattleManager : MonoBehaviour
         _cancelSource.Cancel();
         _cancelSource.Dispose();
     }
-#endregion
-#region Battle
+    #endregion
+    #region Battle
     public void StartBattle()
     {
-        BattleProcess().Forget();
+        _cancelSource = new();
+        BattleProcess(_cancelSource.Token).Forget();
     }
 
-    async UniTask BattleProcess()
+    public void ForceFinishBattle()
     {
-        
+        _cancelSource.Cancel();
+        _cancelSource.Dispose();
+        Debug.LogWarning("[전투 강제 종료됨]");
     }
-#endregion
+
+    private void InitializeBattle()
+    {
+
+    }
+
+    private void SetPlayerUnit()
+    {
+
+    }
+
+    private void SetEnemyUnit()
+    {
+
+    }
+
+    async UniTask BattleProcess(CancellationToken token)
+    {
+    }
+    #endregion
 }
+// await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: token);
+
+[System.Serializable]
+public class BattleData
+{
+    public BattleUnit[] _playerUnit = new BattleUnit[BattleManager.MaxPlayerUnits];
+    public BattleUnit[] _enemyUnit = new BattleUnit[BattleManager.MaxEnemyUnits];
+    // public sound등으로 전투 배경 사운드 넣기.
+
+    // public Action onVictory;
+    // public Action onLoss;
+}
+
+
+// [System.Serializable]
+// public class Sound
+// {
+//     public AudioClip clip;
+//     public float volume = 1.0f;
+//     public float pitch = 1.0f;
+
+//     [HideInInspector] public bool foldout = false;
+// }
--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,8 +5,10 @@ public class TestScript : MonoBehaviour
 {
     [SerializeField] KeyCode muffle;
     [SerializeField] KeyCode runBattle;
+    [SerializeField] KeyCode finishBattle;
     void Update()
     {
-        if(Input.GetKeyDown(runBattle)) BattleManager.Instance.StartBattle();
+        if (Input.GetKeyDown(runBattle)) BattleManager.Instance.StartBattle();
+        if (Input.GetKeyDown(finishBattle)) BattleManager.Instance.ForceFinishBattle();
     }
 }
